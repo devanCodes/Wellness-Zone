fd6dc59a0e4de4f7a32768c1864e36e9335822c2
devanCodes <devanghailey@gmail.com>
2023-09-18 16:08:46 -0400

    TransformationSlideshow height update

--- src/components/TransformationSlideshow.jsx
+++ src/components/TransformationSlideshow.jsx
@@ -41,7 +41,7 @@ const TransformationSlideshow = () => {
         <img
           src={currentPhoto}
           alt={`Photo ${currentSlide + 1}`}
-          className="w-5/6 h-screen mx-auto my-8 flex transition-transform duration-1000 min-h-[500px] max-h-[500px]"
+          className="w-5/6 h-screen mx-auto my-8 flex transition-transform duration-1000 min-h-[300px] max-h-[300px]"
         />
       </div>
       <div className="absolute flex top-0 right-8 space-x-1">
